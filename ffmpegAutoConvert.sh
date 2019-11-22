c654fe0946e627999375e793c455adf22d98b42d
sam brysbaert <sam.brysbaert@gmail.com>
2019-11-22 20:44:36 +0100

    now working as intended as far as I can tell

--- ffmpegAutoConvert.sh
+++ ffmpegAutoConvert.sh
@@ -2,14 +2,32 @@
 
 inputDir="/hdd/filmsUnconverted"
 outputDir="/hdd/filmsConverted"
+
+# locations of executables on system, change this to match the locations on your personal system
 mediainfo="/run/current-system/sw/bin/mediainfo"
 ffmpeg="/run/current-system/sw/bin/ffmpeg"
 
+# if this isn't set, then globs are evaluated literally if no matching files are found, this can cause errors later on in the program, e.g. with ffmpeg
 shopt -s nullglob
+# this makes it possible to glob into directories
+shopt -s globstar
 
-for file in $inputDir/*.{mkv,mp4,avi}; do
+for file in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv}; do
 	format="$($mediainfo --Inform="Video;%Format%" "$file")"
 	if [ "$format" = "AVC" ]; then
-		$ffmpeg -y -ss 500 -i "$file" -t 20 -c copy "$outputDir/$(basename ${file%.*}).mp4"
+		videoSettings="-c:v copy"
+	else
+		videoSettings="-c:v libx264 -preset ultrafast -crf 22"
 	fi
+
+	audioSettings="-c:a aac -ac 2"
+	subtitleSettings="-c:s mov_text"
+
+	relativeFileName="${file#"$inputDir"/}"
+	outputFile=""$outputDir"/"${relativeFileName%.*}".mp4"
+	dirToCreate="${outputFile%/*}"
+	
+	mkdir -p "$dirToCreate" 
+
+	$ffmpeg -n -i "$file" -movflags faststart $videoSettings $audioSettings $subtitleSettings -map 0 "$outputFile"
 done
