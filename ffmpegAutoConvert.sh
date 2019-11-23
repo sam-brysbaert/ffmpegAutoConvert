8a60cda97d3a6e9bddea5aebdbd62e19394c20b8
sam brysbaert <sam.brysbaert@gmail.com>
2019-11-23 08:39:48 +0100

    added copying of srt files + changed executable locations to absolute

--- ffmpegAutoConvert.sh
+++ ffmpegAutoConvert.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-inputDir="/hdd/filmsUnconverted"
-outputDir="/hdd/filmsConverted"
+inputDir="$1"
+outputDir="$2"
 
 # locations of executables on system, change this to match the locations on your personal system
 mediainfo="/run/current-system/sw/bin/mediainfo"
 ffmpeg="/run/current-system/sw/bin/ffmpeg"
+mkdir="/run/current-system/sw/bin/mkdir"
+cp="/run/current-system/sw/bin/cp"
 
 # if this isn't set, then globs are evaluated literally if no matching files are found, this can cause errors later on in the program, e.g. with ffmpeg
 shopt -s nullglob
@@ -25,9 +27,10 @@ for file in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv}; do
 
 	relativeFileName="${file#"$inputDir"/}"
 	outputFile=""$outputDir"/"${relativeFileName%.*}".mp4"
-	dirToCreate="${outputFile%/*}"
+	dirToCreate="$(dirname "$outputFile")"
 	
-	mkdir -p "$dirToCreate" 
+	$mkdir -p "$dirToCreate" 
+	$cp $(dirname "$file")/*.srt $dirToCreate
 
 	$ffmpeg -n -i "$file" -movflags faststart $videoSettings $audioSettings $subtitleSettings -map 0 "$outputFile"
 done
