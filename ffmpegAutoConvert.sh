1f3c48fe8ca55fcf08a64afe22a1b18379861548
sam brysbaert <sam.brysbaert@gmail.com>
2019-11-25 19:42:20 +0100

    added some error handling for faulty arguments

--- ffmpegAutoConvert.sh
+++ ffmpegAutoConvert.sh
@@ -3,6 +3,29 @@
 inputDir="$1"
 destinationDir="$2"
 
+showError() {
+	echo "$1" 1>&2
+	exit 1
+}
+
+# check if the correct number of arguments have been given
+
+if [ "$#" -ne 2 ]; then
+	showError "ERROR: Please provide exactly two directories as arguments. The first being the input directory (where the files you want to convert are located), the second being the output directory (where the converted files will be placed).
+Like so: ffmpegAutoConvert /path/to/input/dir /path/to/output/dir"
+fi
+
+# check if inputDir exists
+if [ ! -d "$inputDir" ]; then
+	showError "ERROR: Input directory does not exist."
+fi
+
+# make destination directory if it doesn't already exist
+mkdir -p $destinationDir
+if [ $? -eq 1 ]; then
+    showError "ERROR: Error creating target directory."
+fi
+
 # if this isn't set, then globs are evaluated literally if no matching files
 # are found, which we don't want for the main for-loop.
 shopt -s nullglob
