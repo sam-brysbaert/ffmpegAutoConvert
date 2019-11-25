ca7ef0ef2201fdab224cae6d0dba7082a8b3bdb0
sam brysbaert <sam.brysbaert@gmail.com>
2019-11-25 16:09:35 +0100

    some more refactoring

--- ffmpegAutoConvert.sh
+++ ffmpegAutoConvert.sh
@@ -1,15 +1,26 @@
 #!/usr/bin/env bash
 
 inputDir="$1"
-outputDir="$2"
+destinationDir="$2"
 
 # if this isn't set, then globs are evaluated literally if no matching files
-# are found.
+# are found, which we don't want.
 shopt -s nullglob
 # this makes it possible to glob into directories
 shopt -s globstar
 
-for file in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv,m4v}; do
+for inputFile in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv,m4v}; do
+
+	# calculate path for output file
+	relativeFilename="${inputFile#"$inputDir"/}"
+	outputFile=""$destinationDir"/"${relativeFilename%.*}".mp4"
+
+	# create directory for output file
+	outputDir="$(dirname "$outputFile")"
+	mkdir -p "$outputDir"
+
+	# copy potential srt files
+	cp $(dirname "$inputFile")/*.srt $outputDir
 
 	# stops wildcard chars from being interpreted as globs.
 	shopt -u nullglob
@@ -19,8 +30,8 @@ for file in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv,m4v}; do
 	# for quicker start of video when streaming
 	options+="-movflags faststart "
 
-	# video
-	formatVid="$(mediainfo --Inform="Video;%Format%" "$file")"
+	# determine video settings
+	formatVid="$(mediainfo --Inform="Video;%Format%" "$inputFile")"
 	if [ "$formatVid" = "AVC" ]; then
 		options+="-c:v copy "
 	else
@@ -28,29 +39,18 @@ for file in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv,m4v}; do
 	fi
 	options+="-map 0:v:0 "
 
-	# audio
+	# determine audio settings
 	options+="-c:a aac -ac 2 "
 	options+="-map 0:a? "
 
-	# subtitles
-	formatSub="$(mediainfo --Inform="Text;%Format%" "$file")"
+	# determine subtitle settings
+	formatSub="$(mediainfo --Inform="Text;%Format%" "$inputFile")"
 	if [[ $formatSub != *"PGS"* && $formatSub != *"VobSub"* ]]; then
 		options+="-c:s mov_text "
 		options+="-map 0:s? "
 	fi
-
-	# calculate path for output file
-	relativeFileName="${file#"$inputDir"/}"
-	outputFile=""$outputDir"/"${relativeFileName%.*}".mp4"
-
-	# create directory for output file
-	dirToCreate="$(dirname "$outputFile")"
-	mkdir -p "$dirToCreate" 
-
-	# copy potential srt files
-	cp $(dirname "$file")/*.srt $dirToCreate
 	
 	# execute ffmpeg
-	ffmpeg -n -i "$file" $options "$outputFile"
+	ffmpeg -n -i "$inputFile" $options "$outputFile"
 
 done
