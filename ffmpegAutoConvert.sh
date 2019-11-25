727f92cdf7b2685998b8db527a966260b258c488
sam brysbaert <sam.brysbaert@gmail.com>
2019-11-25 15:53:33 +0100

    refactored entire script

--- ffmpegAutoConvert.sh
+++ ffmpegAutoConvert.sh
@@ -3,37 +3,54 @@
 inputDir="$1"
 outputDir="$2"
 
-# if this isn't set, then globs are evaluated literally if no matching files are found, this can cause errors later on in the program, e.g. with ffmpeg
+# if this isn't set, then globs are evaluated literally if no matching files
+# are found.
 shopt -s nullglob
 # this makes it possible to glob into directories
 shopt -s globstar
 
 for file in $inputDir/**/*.{mkv,mp4,avi,m4a,flv,mov,wmv,m4v}; do
+
+	# stops wildcard chars from being interpreted as globs.
+	shopt -u nullglob
+
+	options=""
+
+	# for quicker start of video when streaming
+	options+="-movflags faststart "
+
+	# video
 	formatVid="$(mediainfo --Inform="Video;%Format%" "$file")"
 	if [ "$formatVid" = "AVC" ]; then
-		videoSettings="-c:v copy"
+		options+="-c:v copy "
 	else
-		videoSettings="-c:v libx264 -preset ultrafast -crf 22"
+		options+="-c:v libx264 -preset ultrafast -crf 22 "
+	fi
+	options+="-map 0:v:0 "
+
+	# audio
+	options+="-c:a aac -ac 2 "
+	options+="-map 0:a? "
+
+	# subtitles
+	formatSub="$(mediainfo --Inform="Text;%Format%" "$file")"
+	if [[ $formatSub != *"PGS"* && $formatSub != *"VobSub"* ]]; then
+		options+="-c:s mov_text "
+		options+="-map 0:s? "
 	fi
 
-	vidMapSettings="0:v:0"
-
-	audioSettings="-c:a aac -ac 2"
-	audioMapSettings="0:a?"
-
+	# calculate path for output file
 	relativeFileName="${file#"$inputDir"/}"
 	outputFile=""$outputDir"/"${relativeFileName%.*}".mp4"
+
+	# create directory for output file
 	dirToCreate="$(dirname "$outputFile")"
-	
 	mkdir -p "$dirToCreate" 
+
+	# copy potential srt files
 	cp $(dirname "$file")/*.srt $dirToCreate
 	
-	formatSub="$(mediainfo --Inform="Text;%Format%" "$file")"
-	if [[ $formatSub = *"PGS"* || $formatSub = *"VobSub"* ]]; then
-		ffmpeg -n -i "$file" -movflags faststart $videoSettings $audioSettings -map $vidMapSettings -map "$audioMapSettings" "$outputFile"
-	else
-		subtitleSettings="-c:s mov_text"
-		subMapSettings="0:s?"
-		ffmpeg -n -i "$file" -movflags faststart $videoSettings $audioSettings $subtitleSettings -map $vidMapSettings -map "$audioMapSettings" -map "$subMapSettings" "$outputFile"
-	fi
+	# execute ffmpeg
+	ffmpeg -n -i "$file" $options "$outputFile"
+
 done
